30395c7118f3eda6e2cb0998149a84ca88795a6c
Pushpinder <pushpinder.kaur@revature.com>
2021-04-09 11:39:52 -0400

    Repository design Pattern part 2 - Create and read

--- 2-Sql/HeroesApp/HeroesConsoleUI/Program.cs
+++ 2-Sql/HeroesApp/HeroesConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using HeroesLib;
 
 namespace HeroesConsoleUI
 {
@@ -8,9 +9,42 @@ namespace HeroesConsoleUI
     {
         static void Main(string[] args)
         {
+            IRepository repository=Dependencies.CreateRepository();
+         
             Console.WriteLine("Fetching Super Hero");
+            RunUI(repository);
           
         }
-       
+       public static void RunUI(IRepository repository){
+           /*var SuperHeroes = repository.GetAllSuperHeros();
+           foreach (var superhero in SuperHeroes)
+           {
+               Console.WriteLine($"{superhero.Id} {superhero.Alias} {superhero.RealName}  {superhero.HideOut}");
+           }*/
+
+          /* Console.WriteLine("Please enter the name of Super hero you are looking to search for");
+           var name=Console.ReadLine();
+           var superhero= repository.GetSuperHeroByName(name);
+           Console.WriteLine($"{superhero.Id} {superhero.Alias} {superhero.RealName}  {superhero.HideOut}");*/
+           Console.Write("Please enter the SuperHero alias");
+           string _alias = Console.ReadLine();
+           Console.Write("Please enter the SuperHero real name");
+           string _realName = Console.ReadLine();
+           Console.Write("Please enter the SuperHero hide out place");
+           string _hideOut = Console.ReadLine();
+
+           HeroesLib.Models.SuperHero superHero= new HeroesLib.Models.SuperHero{
+              Alias=_alias,
+              HideOut=_hideOut,
+              RealName=_realName
+           };
+           repository.Add(superHero);
+           Console.WriteLine( $"Super hero {_alias} is added"   );
+           var SuperHeroes = repository.GetAllSuperHeros();
+           foreach (var superhero in SuperHeroes)
+           {
+               Console.WriteLine($"{superhero.Id} {superhero.Alias} {superhero.RealName}  {superhero.HideOut}");
+           }
+       }
     }
 }
