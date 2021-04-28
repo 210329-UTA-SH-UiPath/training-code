5d541227f2c2732a3a98191de17649d711ac8910
Pushpinder <pushpinder.kaur@revature.com>
2021-04-28 10:31:40 -0400

    Fixed the Model Validation and Connection String removed from Data

--- 4-Services/HeroApp/HeroData/Entities/SuperHeroContext.cs
+++ 4-Services/HeroApp/HeroData/Entities/SuperHeroContext.cs
@@ -19,13 +19,6 @@ namespace HeroData.Entities
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Server=tcp:heroesapp.database.windows.net,1433;Initial Catalog=SuperHerosDb;Persist Security Info=False;User ID=dev;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False");
-            }
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SuperHero>().HasData(new SuperHero(1, "Peter Parker", "Spiderman", "His study room"));
