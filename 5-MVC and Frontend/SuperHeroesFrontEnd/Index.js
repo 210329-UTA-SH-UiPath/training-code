48bf47599b98c4ab1fec0fa6562e11b639df0cd3
Kirby Chan <kirbyrchan@gmail.com>
2021-05-03 18:15:50 -0400

    Added JS functions for GetAllSuperHeroes and AddSuperHero

--- 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
+++ 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
@@ -1,8 +1,56 @@
-function GetAllSuperHeroes(){
+function GetAllSuperHeroes() {
+    let superheroes = {};
 
-    //write your logic here
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            superheroes = JSON.parse(xhr.responseText);
+            let heroTBody = document.querySelector('#heroes tbody');
+            json.forEach(function (obj) {
+                console.log(obj.Id);
+                let newRow = heroTBody.insertRow();
+
+                let newCell = newRow.insertCell();
+                let newText = document.createTextNode(`${obj.RealName}`);
+                newCell.appendChild(newText);
+
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(`${obj.Alias}`);
+                newCell.appendChild(newText);
+
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(`${obj.HideOut}`);
+                newCell.appendChild(newText);
+            });
+        }
+    };
+
+    xhr.open('GET', 'https://localhost:44315/api/superheroDb/', true);
+
+    xhr.send();
+
 }
 
-function AddSuperHero(){
-    //write your logic here
+function AddSuperHero() {
+    let realNameInput = document.querySelector('#realName').value;
+    let aliasInput = document.querySelector('#alias').value;
+    let hideOutInput = document.querySelector('#hideOut').value;
+
+    let xhr = new XMLHttpRequest();
+    let url = "submit.php";
+
+    xhr.open("POST", 'https://localhost:44315/api/superheroDb/', true);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            result.innerHTML = this.responseText
+        }
+    };
+
+    var data = JSON.stringify({ "RealName": realNameInput, "Alias": aliasInput, "HideOut": hideOutInput });
+
+    xhr.send(data);
 }
