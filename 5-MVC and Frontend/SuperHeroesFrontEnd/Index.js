2fb65c6511e894c8a5533185b6c2300a0c7c024c
Jeffrey Breiner <jeffreybreiner99@gmail.com>
2021-05-04 09:06:37 -0400

    My attempt at adding JS functionality

--- 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
+++ 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
@@ -1,8 +1,52 @@
 function GetAllSuperHeroes(){
+    let APILink = `https://localhost:44315/api/SuperHero`;
+    let superHeros = {};
+    let xhr = new XMLHttpRequest();
 
-    //write your logic here
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            superHeros = JSON.parse(xhr.responseText);
+            let SuperHerosTableBody = document.querySelector('#heroes tbody');
+
+            json.forEach(function (obj) {
+                console.log(obj.Id);
+                let newRow = heroTBody.insertRow();
+
+                let newCell = newRow.insertCell();
+                let newText = document.createTextNode(`${obj.RealName}`);
+                newCell.appendChild(newText);
+
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(`${obj.Alias}`);
+                newCell.appendChild(newText);
+
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(`${obj.HideOut}`);
+                newCell.appendChild(newText);
+            });
+        }
+    };
+
+    xhr.open('GET', 'https://localhost:44315/api/superheroDb/', true);
+    xhr.send();
 }
 
-function AddSuperHero(){
-    //write your logic here
+function AddSuperHero() {
+    let realNameInput = document.querySelector('#realName').value;
+    let aliasInput = document.querySelector('#alias').value;
+    let hideOutInput = document.querySelector('#hideOut').value;
+    let xhr = new XMLHttpRequest();
+    let url = "submit.php";
+
+    xhr.open("POST", 'https://localhost:44315/api/superheroDb/', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            result.innerHTML = this.responseText
+        }
+    };
+
+    var data = JSON.stringify({ "RealName": realNameInput, "Alias": aliasInput, "HideOut": hideOutInput });
+    xhr.send(data);
 }
