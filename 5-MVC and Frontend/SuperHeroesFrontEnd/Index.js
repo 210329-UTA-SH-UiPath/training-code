ebbf71cb39d677d4ce7af3436c945badcbaa65a0
Miles Plurad <pluradmiles@gmail.com>
2021-05-04 05:08:48 -0500

    Added GET and POST, Unsure how to test

--- 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
+++ 5-MVC and Frontend/SuperHeroesFrontEnd/Index.js
@@ -1,8 +1,39 @@
-function GetAllSuperHeroes(){
+function GetAllSuperHeroes() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://localhost:44315/api/superheroDb/', true);
 
-    //write your logic here
+    // request state change event
+    xhr.onreadystatechange = function () {
+
+        // request completed?
+        if (xhr.readyState !== 4) return;
+
+        if (xhr.status === 200) {
+            // request successful - show response
+            console.log(xhr.responseText);
+        }
+        else {
+            // request error
+            console.log('HTTP error', xhr.status, xhr.statusText);
+        }
+    };
+
+    // start request
+    xhr.send();
 }
 
-function AddSuperHero(){
+function AddSuperHero(superhero) {
     //write your logic here
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'https://localhost:44315/api/superheroDb/', true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            // Request finished. Do processing here.
+        }
+    }
+    xhr.send(superhero);
 }
